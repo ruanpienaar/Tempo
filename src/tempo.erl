4af6aa3ea0ae30265b1c78e198f739c1bb1399ec
ruanpienaar <ruan800@gmail.com>
2017-10-30 00:34:28 +0000

    Better Demo

--- src/tempo.erl
+++ src/tempo.erl
@@ -1,65 +1,95 @@
 -module(tempo).
 
 -export([
-    low_rate_demo/0,
-    fast_rate_demo/0,
-    start_link/2,
-    init/2
+    start_link/3,
+    init/3
 ]).
 
-start_link(LimitRef, LimitPSec) ->
-    proc_lib:start_link(?MODULE, init, [LimitRef, LimitPSec]).
+-export([
+    low_rate_demo/0,
+    fast_rate_demo/0,
+    very_fast_rate_demo/0
+]).
 
-init(LimitRef, LimitPSec) ->
+start_link(LimitRef, Limit, Interval) ->
+    proc_lib:start_link(?MODULE, init, [LimitRef, Limit, Interval]).
+
+init(LimitRef, Limit, Interval) ->
     Tid = ets:new(LimitRef, [set, private]),
     true = ets:insert(Tid, {LimitRef, 0}),
-    {ok, TRef} = timer:send_interval(1000, cleanup),
-    loop_init(Tid, LimitRef, LimitPSec, TRef).
+    {ok, TRef} = timer:send_interval(Interval, cleanup),
+    loop_init(Tid, LimitRef, Limit, TRef, Interval).
 
-loop_init(Tid, LimitRef, LimitPSec, TRef) ->
+loop_init(Tid, LimitRef, Limit, TRef, Interval) ->
     proc_lib:init_ack(self()),
-    loop(Tid, LimitRef, LimitPSec, TRef).
+    loop(Tid, LimitRef, Limit, TRef, Interval).
 
-loop(Tid, LimitRef, LimitPSec, TRef) ->
+loop(Tid, LimitRef, Limit, TRef, Interval) ->
     receive
+        cleanup ->
+            % io:format("C~p~n", [erlang:monotonic_time(nano_seconds)]),
+            true = ets:insert(Tid, {LimitRef, 0}),
+            loop(Tid, LimitRef, Limit, TRef, Interval);
         {ReqPid, hit} ->
             case ets:update_counter(Tid, LimitRef, 1) of
-                NewCount when NewCount >= LimitPSec+1 ->
+                % Plus 1 here, cause we already updated, and did not
+                % lookup before we updated...
+                NewCount when NewCount >= Limit + 1 ->
                     % io:format("LC ~p~n", [NewCount]),
                     % io:format("L ~p~n", [erlang:monotonic_time(nano_seconds)]),
                     ReqPid ! limit;
-                _NewCount ->
+                NewCount ->
                     % io:format("HC ~p~n", [NewCount]),
                     % io:format("H ~p~n", [erlang:monotonic_time(nano_seconds)]),
                     ReqPid ! ok
             end,
-            loop(Tid, LimitRef, LimitPSec, TRef);
-        cleanup ->
-            % io:format("C~p~n", [erlang:monotonic_time(nano_seconds)]),
-            true = ets:insert(Tid, {LimitRef, 0}),
-            loop(Tid, LimitRef, LimitPSec, TRef)
-    after
-        1005 ->
-            exit(timer_failed)
+            loop(Tid, LimitRef, Limit, TRef, Interval)
+    % after
+    %     Interval ->
+    %         exit(timer_failed)
     end.
 
-%% Just uncomment the above io:format's for more info when running the demo's.
+%% Uncomment the above io:format's for more info when running the demo's.
+% Send 'send_something' every 1000ms/1sec, rate limited to 1 per s,
+% Should see only 10
 low_rate_demo() ->
-    Pid = tempo:start_link(countme, 1),
-    timer:sleep(10),
-    [ begin
-        timer:sleep(1001),
-        Pid ! {self(), hit}
-      end || _X <- lists:seq(1, 5)
-    ],
-    erlang:exit(Pid, kill).
+    Pid = tempo:start_link(countme, 1, 1000),% 1 Per second
+    SPid = spawn(fun() -> sender_loop(Pid, 0) end),
+    {ok, TRef} = timer:send_interval(1000, SPid, send_something),
+    erlang:send_after(10010, SPid, done).
 
+% sending 5 per second for 10 seconds
+% Should see only 10
 fast_rate_demo() ->
-    Pid = tempo:start_link(countme, 1),
-    timer:sleep(10),
-    [ begin
-        timer:sleep(101),
-        Pid ! {self(), hit}
-      end || _X <- lists:seq(1, 100)
-    ],
-    erlang:exit(Pid, kill).
+    Pid = tempo:start_link(countme, 1, 1000),% 1 Per second
+    SPid = spawn(fun() -> sender_loop(Pid, 0) end),
+    {ok, TRef} = timer:send_interval(200, SPid, send_something),
+    erlang:send_after(10000, SPid, done).
+
+% sending 50 per second for 10 seconds
+% Should see only 10
+very_fast_rate_demo() ->
+    Pid = tempo:start_link(countme, 1, 1000),% 1 Per second
+    SPid = spawn(fun() -> sender_loop(Pid, 0) end),
+    {ok, TRef} = timer:send_interval(20, SPid, send_something),
+    erlang:send_after(10000, SPid, done).
+
+
+sender_loop(Pid, C) ->
+    receive
+        send_something ->
+            Pid ! {self(), hit},
+            NC =
+                receive
+                    limit ->
+                        C;
+                    ok ->
+                        CC=C+1,
+                        io:format("~p ", [CC]),
+                        CC
+                end,
+            sender_loop(Pid, NC);
+        done ->
+            erlang:exit(Pid, kill),
+            io:format("~nDONE~n")
+    end.
